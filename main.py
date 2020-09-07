0331480d63a5b157e9b58c96533fa1005fc79a9b
Manuel Aguilar <maguilar15@miners.utep.edu>
2020-09-07 16:35:05 -0600

    upload@Mon Sep  7 16:35:05 MDT 2020

--- main.py
+++ main.py
@@ -4,24 +4,23 @@ from shell.Shell import Shell
 
 if __name__ == "__main__":
     # Environment Variables
+    # Shell Prompt Token
     ps1 = os.getenv("PS1")
-    customShellPrompt = ps1 if ps1 else "?> " # default shell prompt
-
-    # Start Shell Object
-    shell = Shell()
-
+    customShellPrompt = ps1 if ps1 else "?> " # default shell prompt token
     # FileName
     FILENAME = os.getenv("FILENAME")
     shellOutputFileName = FILENAME if FILENAME else "default-out.txt"
 
-    # Feture Flag?
+    # Start Shell Object
+    shell = Shell()
+
+    # Feature Flag?
     quitShell = True
 
-    while quitShell:
+    while True:
         shellPrompt = shell.makeShellPrompt(customShellPrompt)
-        if shellPrompt != "exit":
-            shell.runCommand(shellPrompt)
+        if shellPrompt == "exit":
+            print("Exit Shell, bye")
+            exit()
         else:
-            quitShell = False
-
-    print("Exit Shell, bye")
+            shell.runCommand(shellPrompt)
