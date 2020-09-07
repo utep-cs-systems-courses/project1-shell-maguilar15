6e29b9e0a7b55d78ec1ce7646f4a07412a091a94
Manuel Aguilar <maguilar15@miners.utep.edu>
2020-09-07 15:40:44 -0600

    reduced code@Mon Sep  7 15:40:44 MDT 2020

--- shell/Shell.py
+++ shell/Shell.py
@@ -1,6 +1,6 @@
 # Author: Manuel Aguilar
 
-import os, sys, time, re
+import os, sys, re
 
 
 class Shell(object):
@@ -31,32 +31,43 @@ class Shell(object):
     def runCommand(self,command:str):
         self.pid = os.getpid()
 
-        os.write(1, ("About to fork (pid:%d)\n" % self.pid).encode())
+        os.write(1,"[+]--------------------------------------------[+]\n".encode())
+        os.write(1, f"About to fork (pid:{self.pid})\n".encode())
 
         rc = os.fork()
 
         if rc < 0:
-            os.write(2, ("fork failed, returning %d\n" % rc).encode())
+            os.write(2, f"fork failed, returning {rc}\n".encode())
             sys.exit(1)
 
         elif rc == 0:  # child
-            os.write(1, ("Child: My pid==%d.  Parent's pid=%d\n" %
-                         (os.getpid(), self.pid)).encode())
-            args = command.split(" ")  # COMMAND
-            for dir in re.split(":", os.environ['PATH']):  # try each directory in the path
-                program = "%s/%s" % (dir, args[0])
-                #os.write(1, ("Child:  ...trying to exec %s\n" % program).encode())  PRINTS TO STDOUT
-                try:
-                    os.execve(program, args, os.environ)  # try to exec program
-                except FileNotFoundError:  # ...expected
-                    pass  # ...fail quietly
+            os.write(1, f"Child: My pid=={os.getpid()}.  Parent's pid={self.pid}\n".encode())
+            os.write(1, "--------------------------------------------------\n".encode())
 
-            os.write(2, ("%s: command does not exist\n" % args[0]).encode())
-            sys.exit(1)  # terminate with error
+            _findCommand(command)
 
         else:  # parent (forked ok)
-            os.write(1, ("Parent: My pid=%d.  Child's pid=%d\n" %
-                         (self.pid, rc)).encode())
+            os.write(1, f"Parent: My pid={self.pid}.  Child's pid={rc}\n".encode())
             childPidCode = os.wait()
-            os.write(1, ("Parent: Child %d terminated with exit code %d\n" %
-                         childPidCode).encode())
+
+            os.write(1, "--------------------------------------------------\n".encode())
+            os.write(1, f"Parent: Child {childPidCode[0]} terminated with exit code {childPidCode[1]}\n".encode())
+
+
+def _findCommand(command:str):
+    """
+    Command that will be checked against every directory in $PATH variable.
+    :param command: whatever is typed into the terminal.
+    :return:
+    """
+    args = command.split(" ")  # COMMAND
+    result = [dir for dir in re.split(":",os.environ["PATH"]) if os.path.exists(f"{dir}/{args[0]}")]
+    for e in result:
+        program = f"{e}/{args[0]}"
+
+        os.write(1, "std::out> ".encode())
+        os.execve(program, args, os.environ)  # try to exec program
+    else:
+        os.write(1, "std::err> ".encode())
+        os.write(1,f"{args[0]}: Command does not exist\n".encode())
+
